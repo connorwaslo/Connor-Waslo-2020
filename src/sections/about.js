b4207b4f92821ad3b598fc3e4435833b0d899c14
connorwaslo <connorwaslo@gmail.com>
2019-12-28 12:30:03 -0700

    Styled about, modified corner-icons

--- src/sections/about.js
+++ src/sections/about.js
@@ -9,12 +9,17 @@ export default ({bgColor}) => (
   <FullHeight bgColor={bgColor}>
     <LayoutTextCenter>
       <SectionTitle title='Who am I?'/>
-      <h4 className='about-text display-4'>CS Student @ Vanderbilt</h4>
-      <h1 className='about-text display-4'>Entrepreneur</h1>
-      <h1 className='about-text display-4'>Aspiring marathoner</h1>
-      <h1 className='about-text display-4'>People person</h1>
-      <h1 className='about-text display-4'>CA born, AZ raised</h1>
+      <br/>
+      <h1 className='about-text display-4 my-5'>
+        CS Student @ Vanderbilt
+        <img src={require('../images/inline/anchordown.png')} alt='anchordown'
+             style={{width: '1.5em', marginBottom: 0}}/>
+      </h1>
+      <h1 className='about-text display-4 my-5'>Entrepreneur</h1>
+      <h1 className='about-text display-4 my-5'>Aspiring marathoner</h1>
+      <h1 className='about-text display-4 my-5'>People person</h1>
+      <h1 className='about-text display-4 my-5'>CA born, AZ raised</h1>
     </LayoutTextCenter>
-    <CornerIcon/>
+    <CornerIcon src={require('../images/corner-icons/cactus.png')} alt='cactus'/>
   </FullHeight>
 )
