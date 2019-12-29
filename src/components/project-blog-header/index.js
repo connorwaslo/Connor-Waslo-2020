4956efbeb3c8b9fe3950ed3576abc8f91d8d32b9
connorwaslo <connorwaslo@gmail.com>
2019-12-29 01:00:33 -0700

    Basic project blog header

---
+++ src/components/project-blog-header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './project-blog-header.module.css';
+
+// Todo: When the screen is small, decrease the size of the project image
+function ProjectBlogHeader({children, img, alt}) {
+  return (
+    <React.Fragment>
+      <div className='row my-5'>
+        <div className='col-md-4'>
+          <img src={img} alt={alt} className={`${styles.image} ${styles.center}`}/>
+        </div>
+        <div className='col-md-8'>
+          {children}
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default ProjectBlogHeader;
--- src/pages/automation.js
+++ src/pages/automation.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import LayoutProjectBlog from "../components/layouts/layout-project-blog"
+import ProjectBlogHeader from "../components/project-blog-header"
 
 const Automation = () => (
   <LayoutProjectBlog>
-    <h1>Optimization & Automation</h1>
+    <ProjectBlogHeader img={require('../images/projects/v.png')} alt='vanderbilt'>
+      <h1 className='display-3'>Overhauling Course Registration @ Vanderbilt University</h1>
+      <h1><i>June - November 2019</i></h1>
+    </ProjectBlogHeader>
   </LayoutProjectBlog>
 );
 
