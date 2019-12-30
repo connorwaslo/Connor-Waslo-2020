76a8b31eab9e9771577881a1218f543900f566ac
connorwaslo <connorwaslo@gmail.com>
2019-12-30 13:05:43 -0700

    Interests content

--- src/sections/interests.js
+++ src/sections/interests.js
@@ -6,6 +6,10 @@ function Interests() {
   return (
     <LayoutParallax bgImage={require('../images/backgrounds/happy_place.png')}>
       <SectionTitle title="When I'm not working I enjoy" style={{color: 'white'}}/>
+      <h1 className="text-center text-white my-5">Running 🏃 (it's a love/hate thing)</h1>
+      <h1 className="text-center text-white my-5">Drinking Coffee ☕</h1>
+      <h1 className="text-center text-white my-5">Storytelling 🗣️</h1>
+      <h1 className="text-center text-white my-5">Beach Volleyball 🏐</h1>
     </LayoutParallax>
   )
 }
