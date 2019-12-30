f871c4c96447a7ca2779f48df0299b6b112abf9a
connorwaslo <connorwaslo@gmail.com>
2019-12-30 14:37:04 -0700

    Intro spacing
    Probably temporary depending on background image (if any).

--- src/sections/intro.js
+++ src/sections/intro.js
@@ -7,6 +7,10 @@ export default ({bgColor}) => (
   <FullHeight bgColor={bgColor}>
     <LayoutTextCenter>
       <h1 className='display-1'>Hi, I'm Connor Waslo</h1>
+      <h1 className='my-2 py-3'>.</h1>
+      <h1 className='my-2 py-3'>.</h1>
+      <h1 className='my-2 py-3'>.</h1>
+      <h1 className='my-2 py-3'>.</h1>
       <h1 className='display-3'>Here's what I'm about</h1>
     </LayoutTextCenter>
     <CornerIcon src={require('../images/corner-icons/rocket.png')} alt='rocket'/>
