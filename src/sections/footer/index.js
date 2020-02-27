cadb8748a4ae75dbbe09636844211bc2763bf1dc
connorwaslo <connorwaslo@gmail.com>
2020-02-27 15:40:49 -0600

    Footer date

--- src/sections/footer/index.js
+++ src/sections/footer/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+// Todo: Change the scaling on gatsby depending on mobile or desktop
 function Footer() {
   return (
     <div className='row' style={{overflowX: 'hidden'}}>
       <div className='col-4'>
-        <h3 className='ml-3'>Last Updated: Dec 30, 2019</h3>
+        <h3 className='ml-3'>Last Updated: Feb 27, 2020</h3>
       </div>
       <div className='col-4'>
       </div>
