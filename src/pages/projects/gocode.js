55aa431cfdee53e4baae0d37cefa01d99b909ce1
connorwaslo <connorwaslo@gmail.com>
2020-02-28 18:10:38 -0600

    GoCode content changes

--- src/pages/projects/gocode.js
+++ src/pages/projects/gocode.js
@@ -21,19 +21,22 @@ function GoCode() {
       <BlogText>After brainstorming with a couple mentors, I decided to dive right into this project. I began to teach
         myself mobile app development using <a href="https://facebook.github.io/react-native/" target="_blank">React-Native</a> and spent countless hours
       developing GoCode.</BlogText>
+      <BlogText>I teamed up with close friend and incredible artist, Isabela von Dehl, to tackle the user interface
+        and user experience. Surely version 1 needed to look beautiful right?</BlogText>
       <BlogText>After spending three months and hundreds of hours building the app, I released it on the App Store and
-      Google Play Store. I felt like I had created such a clean, streamlined app. Surely <i>somebody</i> wanted to use it.</BlogText>
+      Google Play Store. I felt like we had created such a clean, streamlined app. Surely <i>somebody</i> wanted to use it.</BlogText>
       <br/>
       <BlogText>Wrong.</BlogText>
       <br/>
       <BlogText>I made probably the most common mistake a wannabe-startup-founder can make. I built a solution without
       really knowing what problem I was solving. I thought that I could build a product and then find the target user for it.</BlogText>
       <BlogText>You can probably see where this is going. I could not find the target user for it.</BlogText>
+      <BlogText>It did get a decent number of users from my school and our sister school, but it wasn't sustainable or replicable.</BlogText>
       <BlogText>As a result I decided to pivot from a mobile app to a website that I intended to license for use in schools.</BlogText>
       <BlogText>This time I actually had conversations with schools before fully building out the website. Unfortunately,
       every school wanted a different website. On top of this huge dilemma, it became apparent that the sales cycle
       for these schools would be brutally slow.</BlogText>
-      <BlogText>I decided to step away from GoCode the summer before my freshman year at Vanderbilt.</BlogText>
+      <BlogText>I chose to step away from GoCode the summer before my freshman year at Vanderbilt.</BlogText>
       <BlogText>Obviously I didn't get the startup that I wanted, but I will never make these mistakes again.</BlogText>
       <br/>
       <BlogText><i>I'll be adding some screenshots and more content to this blog post in the future.</i></BlogText>
