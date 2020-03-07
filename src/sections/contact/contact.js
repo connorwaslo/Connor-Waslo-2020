f4558c451b18f8b591dd7f17c54f6aeb64a0ae99
connorwaslo <connorwaslo@gmail.com>
2020-03-07 14:07:45 -0700

    Add github link at bottom
    Still need to change the picture - waiting til I have internet

--- src/sections/contact/contact.js
+++ src/sections/contact/contact.js
@@ -4,6 +4,8 @@ import SectionTitle from "../../components/section-title"
 import styles from './contact.module.css';
 
 function Contact() {
+
+  // Todo: Change second linkedin photo to be github
   return (
     <LayoutTextCenter>
       <SectionTitle title="Let's Link Up"/>
@@ -16,6 +18,10 @@ function Contact() {
         </a>
         <a href="https://www.linkedin.com/in/connor-waslo/" target="_blank">
           <img src={require('../../images/misc/linkedin.png')} alt="linkedin"
+               className={styles.contactImage}/>
+        </a>
+        <a href="https://www.github.com/connorwaslo" target="_blank">
+          <img src={require('../../images/misc/linkedin.png')} alt="github"
                className={styles.contactImage}/>
         </a>
       </div>
