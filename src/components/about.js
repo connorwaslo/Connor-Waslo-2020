18315a107519f2a31669d53cc6c17a839e861532
connorwaslo <connorwaslo@gmail.com>
2020-05-28 10:59:49 -0700

    Content layout component

--- src/components/about.js
+++ src/components/about.js
@@ -3,6 +3,7 @@ import { ContentContainer, SectionContainer } from "./containers"
 import { MainTitle, AboutText } from "./typography"
 import { Grid } from 'semantic-ui-react'
 import theme from "../styles/theme"
+import ContentGrid from "./layouts/contentGrid"
 
 const AboutPoint = ({ emoji, text }) => (
   <Grid>
@@ -20,23 +21,14 @@ const AboutPoint = ({ emoji, text }) => (
 function About() {
   return (
     <SectionContainer swoosh swooshColor={theme.palette.backgrounds.main} background={theme.palette.accents.green}>
-      <ContentContainer style={{ color: theme.palette.text.white}}>
-        <Grid stackable>
-          <Grid.Row>
-            <Grid.Column width={6}>
-              <MainTitle>About</MainTitle>
-            </Grid.Column>
-            <Grid.Column width={10} style={{ marginTop: theme.spacing(2) }}>
-              <AboutPoint emoji='📚' text='Computer Science and Chinese @ Vanderbilt University'/>
-              <AboutPoint emoji='💻' text='Constantly working on side-projects'/>
-              <AboutPoint emoji='‍‍👨‍💻‍‍' text='Software engineer looking from the business perspective'/>
-              <AboutPoint emoji='🏃‍♂' text='Aspiring marathoner'/>
-              <AboutPoint emoji='👨‍🍳' text='Plant-based wannabe-chef'/>
-              <AboutPoint emoji='🏀' text='Phoenix Suns ride or die'/>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </ContentContainer>
+      <ContentGrid title='About' style={{ color: theme.palette.text.white }} childrenStyle={{ marginTop: theme.spacing(2) }}>
+        <AboutPoint emoji='📚' text='Computer Science and Chinese @ Vanderbilt University'/>
+        <AboutPoint emoji='💻' text='Constantly working on side-projects'/>
+        <AboutPoint emoji='‍‍👨‍💻‍‍' text='Software engineer looking from the business perspective'/>
+        <AboutPoint emoji='🏃‍♂' text='Aspiring marathoner'/>
+        <AboutPoint emoji='👨‍🍳' text='Plant-based wannabe-chef'/>
+        <AboutPoint emoji='🏀' text='Phoenix Suns ride or die'/>
+      </ContentGrid>
     </SectionContainer>
   )
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -2,40 +2,30 @@ import React from 'react'
 import { SectionContainer, ContentContainer } from './containers'
 import theme from '../styles/theme'
 
-import { Grid } from 'semantic-ui-react'
-import { MainTitle } from './typography'
 import Project from "./project"
+import ContentGrid from "./layouts/contentGrid"
 
 function Projects() {
   return (
     <SectionContainer background={theme.palette.backgrounds.main}>
-      <ContentContainer>
-        <Grid stackable>
-          <Grid.Row>
-            <Grid.Column width={6}>
-              <MainTitle>Projects</MainTitle>
-            </Grid.Column>
-            <Grid.Column width={10}>
-              <Project pic='https://via.placeholder.com/150'
-                       title='Scalar'
-                       description='A creative web development agency I founded with friends.'
-                       link='https://scalardev.tech/'/>
-              <Project pic='https://via.placeholder.com/150'
-                       title='Unheard Voices Outreach'
-                       description='Building a mobile app to provide resources and mentorship to formerly incarcerated individuals.'
-                       link=''/>
-              <Project pic='https://via.placeholder.com/150'
-                       title='Athena'
-                       description='Revamping course registration at Vanderbilt University - building a platform based on student input.'
-                       link=''/>
-              <Project pic='https://via.placeholder.com/150'
-                       title='Ignytion'
-                       description='A company I founded which provided data and web automation services to growth-stage startups.'
-                       link=''/>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </ContentContainer>
+      <ContentGrid title='Projects'>
+        <Project pic='https://via.placeholder.com/150'
+                 title='Scalar'
+                 description='A creative web development agency I founded with friends.'
+                 link='https://scalardev.tech/'/>
+        <Project pic='https://via.placeholder.com/150'
+                 title='Unheard Voices Outreach'
+                 description='Building a mobile app to provide resources and mentorship to formerly incarcerated individuals.'
+                 link=''/>
+        <Project pic='https://via.placeholder.com/150'
+                 title='Athena'
+                 description='Revamping course registration at Vanderbilt University - building a platform based on student input.'
+                 link=''/>
+        <Project pic='https://via.placeholder.com/150'
+                 title='Ignytion'
+                 description='A company I founded which provided data and web automation services to growth-stage startups.'
+                 link=''/>
+      </ContentGrid>
     </SectionContainer>
   )
 }
