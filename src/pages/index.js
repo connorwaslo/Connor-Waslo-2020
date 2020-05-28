d95fd4fb82c102faf24c82edc7f57d02d9558fb2
connorwaslo <connorwaslo@gmail.com>
2020-05-28 11:38:20 -0700

    graph for tech stack

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import PageLayout from "../components/layouts/pageLayout"
 import Intro from '../components/intro'
 import About from "../components/about"
 import Projects from "../components/projects"
+import TechGraph from "../components/techGraph"
 
 const IndexPage = () => (
   <PageLayout>
     <Intro/>
     <About/>
     <Projects/>
+    <TechGraph/>
   </PageLayout>
 )
 
